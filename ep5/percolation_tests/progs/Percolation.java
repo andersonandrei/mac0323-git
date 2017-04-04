60986805a11e37b3173653f229dc132113a976a8
Anderson Andrei da Silva <andrei@c.linux.ime.usp.br>
2017-04-04 09:24:06 -0300

    Começando a estruturas as coisas.

---
+++ ep5/percolation_tests/progs/Percolation.java
@@ -0,0 +1,46 @@
+/* Implementar essa budega aqui:
+
+   public class Percolation {
+   public Percolation(int n)                // create n-by-n grid, with all sites initially blocked
+   public void open(int row, int col)       // open the site (row, col) if it is not open already
+   public boolean isOpen(int row, int col)  // is the site (row, col) open?
+   public boolean isFull(int row, int col)  // is the site (row, col) full?
+   public int numberOfOpenSites()           // number of open sites
+   public boolean percolates()              // does the system percolate?
+   public static void main(String[] args)   // unit testing (required)
+   } 
+*/
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+	
+	// create n-by-n grid, with all sites initially blocked
+	public Percolation(int n) {}
+	
+	// open the site (row, col) if it is not open already
+    public void open(int row, int col) {}
+    
+    // is the site (row, col) open?    
+    public boolean isOpen(int row, int col)  {}
+    
+    
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col)  {}
+    
+    
+    // number of open sites
+    public int numberOfOpenSites()  {}
+    
+    
+    // does the system percolate?
+    public boolean percolates() {}
+    
+    
+    // unit testing (required)
+    public static void main(String[] args)   {}
+    
+    
+}
---
+++ ep5/percolation_tests/progs/PercolationStats.java
@@ -0,0 +1,39 @@
+/* Implementar essas budegas
+
+public class PercolationStats {
+   public PercolationStats(int n, int trials)   // perform trials independent experiments on an n-by-n grid
+   public double mean()                         // sample mean of percolation threshold
+   public double stddev()                       // sample standard deviation of percolation threshold
+   public double confidenceLow()                // low  endpoint of 95% confidence interval
+   public double confidenceHigh()               // high endpoint of 95% confidence interval
+}
+*/
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class PercolationStats {
+	
+	// perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {}
+    
+    
+    // sample mean of percolation threshold
+    public double mean() {}
+    
+    
+    // sample standard deviation of percolation threshold
+    public double stddev() {}
+    
+    
+    // low  endpoint of 95% confidence interval                   
+    public double confidenceLow() {}
+    
+    
+    // high endpoint of 95% confidence interval     
+    public double confidenceHigh()  {}
+    
+    
+                 
+}
