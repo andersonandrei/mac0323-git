1974a003c8d782624ab1e43c476d54f730039f1a
Andrei <a.andreisilva@yahoo.com.br>
2017-05-23 20:37:43 -0300

    Doing.

--- ep9/WordFinder.java
+++ ep9/WordFinder.java
@@ -30,21 +30,30 @@ public class WordFinder {
 		StringBuilder word = new StringBuilder();
 		LinkedList<Integer> position = null;
 		char carac = 0;
+		int num;
 		for (int i = 0; i < str.length; i++) {
-			carac = str[i].charAt(0);
-			if (carac == 32) { 
-				position = wf.get(word.toString());
-				if (position == null){
-					position = new LinkedList<Integer>();
+			num = 0;
+			for (int j = 0; j < str[i].length(); j++) {
+				StdOut.println("Olhando pra:    " + str[i]);
+				carac = str[i].charAt(j);
+				StdOut.println("Pegou:   " + carac);
+				if ((carac >= 65 && carac <= 90) || (carac >= 97 && carac <=122)){
+					word.append(carac);
 				}
-				if(!position.contains(i)){
-					position.add(i);
+				else if (carac == 32) {
+					StdOut.println("Formou:    " + word);
+					StdOut.println("Igual a espaço? " + word.equals(" "));
+					position = wf.get(word.toString());
+					if (position == null) {
+						position = new LinkedList<Integer>();
+					}
+					if(!position.contains(i)) {
+						position.add(i);
+					}
+					wf.put(word.toString(), position);
+					word = new StringBuilder();
+					num++;
 				}
-				wf.put(word.toString(), position);
-			}
-
-			if ((carac >= 65 && carac <= 90) || (carac >= 97 && carac <=122)){
-				word.append(carac);
 			}
 		}
 	}
@@ -52,12 +61,13 @@ public class WordFinder {
 	//retorna a palavra que se repete mais vezes nas strings dadas. 
 	//Caso a palavra apareça mais de uma vez na mesma string, ignore.
 	public String getMax() {
+    	if (wf == null) throw new NullPointerException();
 		Iterable<String> words = wf.keys();
 		String str = null;
 		int min = Integer.MIN_VALUE;
 		int tmp;
-
 		for (String s : words) {
+			StdOut.println("No for        :" + s + "->" + wf.get(s).size() );
 			tmp = wf.get(s).size();
 			if (tmp > min) {
 				min = tmp;
@@ -71,6 +81,7 @@ public class WordFinder {
 	//a do vetor quanto na string de índice b. Se não tiver nenhuma, 
 	//retorne null. Se tiver mais de uma, retorne a que quiser.
     public String containedIn(int a, int b) {
+    	if (wf == null) throw new NullPointerException();
     	Iterable<String> words = wf.keys();
     	LinkedList<Integer> position;
     	for (String word : words) {
@@ -86,23 +97,37 @@ public class WordFinder {
 	//do vetor inicial em que ela apareça. Se não aparecer em nenhum, 
 	//retorne um vetor vazio.
     public int[] appearsIn(String str) {
+    	if (wf == null) throw new NullPointerException();
     	LinkedList<Integer> position;
     	int cont = 0;
-    	int v[] = null;
-
+    	int v[] = new int[0];
     	if (wf.contains(str)) {
+    		StdOut.println("Achei a str: " + str);
     		position = wf.get(str);
     		v = new int[position.size()];
     		for (Integer i : position) {
+    			StdOut.println("Position : " + i);
     			v[cont] = i;
+    			StdOut.println("Position cont: " + v[cont]);
     			cont++;
     		}
     	}
     	return v;
     }
 
-    public void main () {
-    	return;
+    public static void main (String[] args) {
+    	String[] str = new String[3];
+    	str[0] = "hoje eu acordei seila ";
+    	str[1] = " oi tudo bem eu seila seila ";
+    	str[2] = " acordei para sorrir e mostrar os dentes hoje ";
+    	WordFinder wf = new WordFinder(str);
+    	StdOut.println("getMax: " + wf.getMax());
+    	StdOut.println("getMax: " + wf.getMax());
+    	StdOut.println("apeearsIn: " + wf.appearsIn("hoje"));
+    	StdOut.println("apeearsIn: " + wf.appearsIn("não"));
+    	StdOut.println("getMax: " + wf.getMax());
+    	StdOut.println("containedIN: " + wf.containedIn(0,1));
+    	StdOut.println("containedIN: " + wf.containedIn(0,2));
     }
 
 }
