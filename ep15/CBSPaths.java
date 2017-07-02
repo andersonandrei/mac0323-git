eaab5e82a39341a5451942667b5ae6e44489d8e2
Andrei <a.andreisilva@yahoo.com.br>
2017-07-02 18:31:29 -0300

    Ep15 "done" and clean.

--- ep15/CBSPaths.java
+++ ep15/CBSPaths.java
@@ -19,25 +19,149 @@ import edu.princeton.cs.algs4.DijkstraSP;
 
 public class CBSPaths {
 	private int n, m, k;
-	private String[] names = new String[6];
+	private String[] names;
+	private int qntNames;
 	private DirectedEdge[] cities;
 	private EdgeWeightedDigraph map;
 	private DijkstraSP[] dijks;
 	private DijkstraSP search;
 	private int[] begin;
 	private int beginMF;
+	private boolean[] secure;
+	private Double[] dists;
+	private Double[] distsMF;
 
-	private CBSPaths (int n, int m, int k) {
-		this.n = n;
-		this.m = m;
-		this.k = k;
-		names[0] = "GRU";
+	private CBSPaths (String str) {
+		In in = new In(str);
+		qntNames = 0;
+/*		names[0] = "GRU";
 		names[1] = "POA";
 		names[2] = "CNF";
 		names[3] = "BSB";
 		names[4] = "GIG";
-		names[5] = "CWB";
-		//names = ["GRU","POA","CNF","BSB","GIG","CWB"];
+		names[5] = "CWB";*/
+
+		this.n = in.readInt();
+		this.m = in.readInt();
+		this.k = in.readInt();
+		this.cities = new DirectedEdge[m];
+		this.begin = new int[k];
+		names = new String[cities()];
+		for (int i = 0; i < cities(); i++){
+			names[i] = " ";
+		}
+		for (int i = 0; i < connections(); i++){
+			String city1, city2;
+			city1 = in.readString();
+			city2 = in.readString();
+			insertCity(city1);
+			insertCity(city2);
+			cities[i] = new DirectedEdge(findIndice(city1), findIndice(city2), in.readDouble());
+		}
+		StdOut.println("Dicionario: ");
+		for (int i = 0; i < cities(); i++){
+			StdOut.println(names[i]);
+		}
+
+		for (int i = 0; i < executives(); i++){
+			begin[i] = findIndice(in.readString());
+		}
+		beginMF = findIndice(in.readString());
+		StdOut.println("Cabou de ler");
+
+		map = new EdgeWeightedDigraph(cities(), 0);
+		for (int i = 0; i < connections(); i++){
+			StdOut.println("From:" + cities[i].to());
+			map.addEdge(cities[i]);
+		}
+
+		StdOut.println("Mostrando");
+		StdOut.println("From:" + map.E());
+		for (DirectedEdge e : map.edges()) {
+			StdOut.println(e.toString());
+		}
+
+		//========================================Prints_Iniciais=
+
+		dijks = new DijkstraSP[executives()];
+		for (int i = 0; i < executives(); i++) {
+			dijks[i] = new DijkstraSP(map, begin[i]);
+		}
+		search = new DijkstraSP(map, beginMF);
+		dists = new Double[cities()];
+		distsMF = new Double[cities()];
+		for (int i = 0; i < cities(); i++){
+			dists[i] = 0.0;
+			distsMF[i] = 0.0;
+		}
+		airMap(dists, distsMF);
+		secure = new boolean[cities()];
+		secure = securePlaces(dists, distsMF);
+	}
+
+	private void insertCity(String city) {
+		StdOut.println("Inserindo: " + city);
+		if (!containCity(city)){
+			StdOut.println("Vai inserir: " + city);
+			names[qntNames] = city;
+			qntNames++;
+		}
+	}
+
+	private boolean containCity(String city) {
+		for(int i = 0; i < cities(); i++) {
+			if (names[i].equals(city)){
+				return true;
+			}
+		};
+		return false;
+	}
+
+	private void airMap(Double[] dists, Double[] distsMF) {
+		for(int i = 0; i < cities(); i++){
+			for (int j = 0; j < executives(); j++){
+				if (dijks[j].distTo(i) > dists[i]){
+					dists[i] = dijks[j].distTo(i);
+				}
+			}
+			distsMF[i] = search.distTo(i);
+		}
+	}
+
+	private boolean[] securePlaces(Double[] dists, Double[] distsMF) {
+		for (int i = 0; i < cities(); i++) {
+			if (dists[i] < distsMF[i]) {
+				secure[i] = true;
+				StdOut.println("aqui: " + findName(i) + " -- dist: " + dists[i]);
+			}
+		}
+		return secure;
+	}
+
+	private boolean hasSecurePlace() {
+		for(int i = 0; i < cities(); i++)
+			if(secure[i]) return true;
+		return false;
+	}
+
+	private void printSecurePlace() {
+		for (int i = 0; i < cities(); i++){
+			if(secure[i])
+				StdOut.println(findName(i));
+		}
+		return;
+	}
+
+	private int cities() {
+		return n;
+	}
+
+	private int connections() {
+		return m;
+	}
+
+	private int executives() {
+		return k;
 	}
 
 	private int findIndice (String s) {
@@ -51,135 +175,11 @@ public class CBSPaths {
 		return names[s];
 	}
 
-	private void airMesh () {
-
-	}
-
 	public static void main(String[] args) {
-		int n, m, k;
-		String filename = args[0];
-        String a;
-        In in = new In(filename);
-		n = in.readInt();
-		m = in.readInt();
-		k = in.readInt();
-		StdOut.println("Olha ai: " + n + " " + m + " " + k);
-		CBSPaths cbs = new CBSPaths(n, m, k);
-		cbs.cities = new DirectedEdge[m];
-		cbs.begin = new int[k];
-		for (int i = 0; i < m; i++){
-			a = in.readString();
-			StdOut.println("Cidade: " + cbs.findIndice(a));
-			cbs.cities[i] = new DirectedEdge(cbs.findIndice(a), cbs.findIndice(in.readString()), in.readDouble());
-		}
-		for (int i = 0; i < k; i++){
-			cbs.begin[i] = cbs.findIndice(in.readString());
-		}
-		cbs.beginMF = cbs.findIndice(in.readString());
-
-		StdOut.println("Cabou de ler");
-
-		cbs.map = new EdgeWeightedDigraph(cbs.n, 0);
-		for (int i = 0; i < m; i++){
-			StdOut.println("From:" + cbs.cities[i].to());
-			cbs.map.addEdge(cbs.cities[i]);
-		}
-
-		StdOut.println("Mostrando");
-		StdOut.println("From:" + cbs.map.E());
-		for (DirectedEdge e : cbs.map.edges()) {
-			StdOut.println(e.toString());
-		}
-
-		//========================================Leituras=
-
-		cbs.dijks = new DijkstraSP[k];
-		for (int i = 0; i < k; i++) {
-			cbs.dijks[i] = new DijkstraSP(cbs.map, cbs.begin[i]);
-		}
-		cbs.search = new DijkstraSP(cbs.map, cbs.beginMF);
-		Double[] dists = new Double[n];
-		Double[] distsMF = new Double[n];
-		for (int i = 0; i < n; i++){
-			dists[i] = 0.0;
-			distsMF[i] = 0.0;
-		}
-
-		for(int i = 0; i < n; i++){
-			for (int j = 0; j < k; j++){
-				if (cbs.dijks[j].distTo(i) > dists[i]){
-					dists[i] = cbs.dijks[j].distTo(i);
-				}
-			}
-			distsMF[i] = cbs.search.distTo(i);
-		}
-
-		//comparisons
-		for (int i = 0; i < n; i++) {
-			if (dists[i] < distsMF[i]) {
-				StdOut.println("aqui: " + cbs.findName(i) + " -- dist: " + dists[i]);
-			}
-		}
-
-		return ;
+		CBSPaths cbs = new CBSPaths(args[0]);
+		//StdOut.println("Olha ai: " + cbs.n + " " + cbs.m + " " + cbs.k);
+		if (cbs.hasSecurePlace()) cbs.printSecurePlace();
+		else StdOut.println("VENHA COMIGO PARA CURITIBA");
+		return;
 	}
-
-	/*for (int i = 1; i < n; i++) {
-					//Se 
-					if (e.distTo(cbs.begin[i]) == Double.POSITIVE_INIFINITY){
-						secure = false;
-					}
-					else {
-
-					}
-				}
-				if (!search.hasPathTo(e.from()))
-					securePlace.enqueue(e);*/
-
-/*
-
-StdOut.println("Partindo inicialmente de: " + cbs.begin[0]);
-		//We have to take the incidents of execs. 
-		for (DirectedEdge e : cbs.map.adj(cbs.begin[0])) {
-		//for (DirectedEdge e : cbs.dijks[0].pathTo(0)) {
-			StdOut.println("indo de e para : " + e.to());
-			for (DirectedEdge fromE = e; fromE.to() != 0 || fromE != e; fromE = cbs.cities[e.to()]){//: cbs.map.adj(e.to())) {
-				StdOut.println("prox de prox : " + fromE.to());
-			}
-			//Take the other incidents of the others execs.
-			for (int i = 1; i < k; i++){
-				StdOut.println("Olhando pro k-esimo exec: " + i + " begin: " + cbs.begin[i]);
-				for (DirectedEdge f : cbs.map.adj(cbs.begin[i])) {
-				StdOut.println("indo de f para : " + f.to());
-				//for (DirectedEdge f : cbs.dijks[i].pathTo(i)) {
-					//Verify if we can go e -> f.
-					if (cbs.dijks[0].distTo(f.from()) == Double.POSITIVE_INFINITY){
-						dists[f.from()] = Double.POSITIVE_INFINITY;
-					}
-					else {
-						//Check the distances point to point and change in the vector.
-						if (cbs.dijks[i].distTo(f.from()) > cbs.dijks[0].distTo(f.from())){
-							if (cbs.dijks[i].distTo(f.from()) > dists[f.from()]){
-								dists[f.from()] = cbs.dijks[i].distTo(f.from());
-							}
-						}
-						else {
-							if (cbs.dijks[0].distTo(f.from()) > dists[f.from()]){
-								dists[f.from()] = cbs.dijks[0].distTo(f.from());
-							}
-						}
-					}
-				}
-				i++;
-			}
-		}
-
-
-		StdOut.println("Bostão");
-		for (int i = 0; i < n; i++){
-			StdOut.println(dists[i]);
-		}
-
-*/
-
 }
