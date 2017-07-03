8d31d0f5c403363f6cf679da93b936ffcfd98ac6
Andrei <a.andreisilva@yahoo.com.br>
2017-07-03 17:43:34 -0300

    Finding bugs

--- ep15/CBSPaths.java
+++ ep15/CBSPaths.java
@@ -1,56 +1,40 @@
-/*
-	Observações interessantes.
-	Casos de busca da MF à Execs.
-	Caso 1: MF tem origem atrás de uma pos de Exec.
-		=> Todos os destinos possiveis dos exec são sucesso!
-			PS: O exemplo do paca é esse caso .
-	Caso 2: Origens nada a ver um com o outro, ai precisamos dos 
-		caminhos mínimos.
-*/
-
+import java.lang.IllegalArgumentException;
+import java.lang.NullPointerException;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-import java.lang.NullPointerException;
 import edu.princeton.cs.algs4.In;
-import java.lang.IllegalArgumentException;
 import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 import edu.princeton.cs.algs4.DirectedEdge;
 import edu.princeton.cs.algs4.DijkstraSP;
+import edu.princeton.cs.algs4.MinPQ;
 
 public class CBSPaths {
 	private int n, m, k;
-	private String[] names;
-	private int qntNames;
+	private String[] names; //name of the cities
+	private int qntNames; //Quantity of cities
 	private DirectedEdge[] cities;
-	private EdgeWeightedDigraph map;
+	private EdgeWeightedDigraph map; //map with the DirectEdges cities
 	private DijkstraSP[] dijks;
-	private DijkstraSP search;
-	private int[] begin;
-	private int beginMF;
+	private DijkstraSP search; //Min dists vector to MF
+	private int[] begin; //Cities that the executives begin
+	private int beginMF; //city that MF begin
 	private boolean[] secure;
 	private Double[] dists;
 	private Double[] distsMF;
 
 	private CBSPaths (String str) {
 		In in = new In(str);
-		qntNames = 0;
-/*		names[0] = "GRU";
-		names[1] = "POA";
-		names[2] = "CNF";
-		names[3] = "BSB";
-		names[4] = "GIG";
-		names[5] = "CWB";*/
-
 		this.n = in.readInt();
 		this.m = in.readInt();
 		this.k = in.readInt();
 		this.cities = new DirectedEdge[m];
 		this.begin = new int[k];
 		names = new String[cities()];
+		qntNames = 0;
 		for (int i = 0; i < cities(); i++){
 			names[i] = " ";
 		}
-		for (int i = 0; i < connections(); i++){
+		for (int i = 0; i < connections(); i++){ //Construct connections
 			String city1, city2;
 			city1 = in.readString();
 			city2 = in.readString();
@@ -58,33 +42,16 @@ public class CBSPaths {
 			insertCity(city2);
 			cities[i] = new DirectedEdge(findIndice(city1), findIndice(city2), in.readDouble());
 		}
-		StdOut.println("Dicionario: ");
-		for (int i = 0; i < cities(); i++){
-			StdOut.println(names[i]);
-		}
-
-		for (int i = 0; i < executives(); i++){
+		for (int i = 0; i < executives(); i++){ //Where the executives begin
 			begin[i] = findIndice(in.readString());
 		}
 		beginMF = findIndice(in.readString());
-		StdOut.println("Cabou de ler");
-
 		map = new EdgeWeightedDigraph(cities(), 0);
-		for (int i = 0; i < connections(); i++){
-			StdOut.println("From:" + cities[i].to());
+		for (int i = 0; i < connections(); i++){ //Construct map of EdgeWeightedDigraph
 			map.addEdge(cities[i]);
 		}
-
-		StdOut.println("Mostrando");
-		StdOut.println("From:" + map.E());
-		for (DirectedEdge e : map.edges()) {
-			StdOut.println(e.toString());
-		}
-
-		//========================================Prints_Iniciais=
-
 		dijks = new DijkstraSP[executives()];
-		for (int i = 0; i < executives(); i++) {
+		for (int i = 0; i < executives(); i++) { //Construct DijkstraSP[] to take the min(s) dist(s)
 			dijks[i] = new DijkstraSP(map, begin[i]);
 		}
 		search = new DijkstraSP(map, beginMF);
@@ -99,15 +66,15 @@ public class CBSPaths {
 		secure = securePlaces(dists, distsMF);
 	}
 
+	//Insert a city into the dictionary
 	private void insertCity(String city) {
-		StdOut.println("Inserindo: " + city);
 		if (!containCity(city)){
-			StdOut.println("Vai inserir: " + city);
 			names[qntNames] = city;
 			qntNames++;
 		}
 	}
 
+	//Verify if the city alredy exist
 	private boolean containCity(String city) {
 		for(int i = 0; i < cities(); i++) {
 			if (names[i].equals(city)){
@@ -117,6 +84,7 @@ public class CBSPaths {
 		return false;
 	}
 
+	//Calculate the dists with DijkstraSP
 	private void airMap(Double[] dists, Double[] distsMF) {
 		for(int i = 0; i < cities(); i++){
 			for (int j = 0; j < executives(); j++){
@@ -128,22 +96,24 @@ public class CBSPaths {
 		}
 	}
 
+	//Mark the secure places
 	private boolean[] securePlaces(Double[] dists, Double[] distsMF) {
 		for (int i = 0; i < cities(); i++) {
 			if (dists[i] < distsMF[i]) {
 				secure[i] = true;
-				StdOut.println("aqui: " + findName(i) + " -- dist: " + dists[i]);
 			}
 		}
 		return secure;
 	}
 
+	//Verify if exist secure place
 	private boolean hasSecurePlace() {
 		for(int i = 0; i < cities(); i++)
 			if(secure[i]) return true;
 		return false;
 	}
 
+	//Print the secure place
 	private void printSecurePlace() {
 		for (int i = 0; i < cities(); i++){
 			if(secure[i])
@@ -177,8 +147,19 @@ public class CBSPaths {
 
 	public static void main(String[] args) {
 		CBSPaths cbs = new CBSPaths(args[0]);
-		//StdOut.println("Olha ai: " + cbs.n + " " + cbs.m + " " + cbs.k);
-		if (cbs.hasSecurePlace()) cbs.printSecurePlace();
+		//LinearProbingHashST<String, Integer> st = new LinearProbingHashST<String, Integer>();
+		MinPQ<String> st = new MinPQ<String>();
+		if (cbs.hasSecurePlace()) {
+			for (int i = 0; i < cbs.cities(); i++){
+				if(cbs.secure[i])
+					st.insert(cbs.findName(i));
+					//StdOut.println(cbs.findName(i));
+			}
+			while (!st.isEmpty()) {
+				StdOut.println(st.delMin());
+			}
+			//cbs.printSecurePlace();
+		}
 		else StdOut.println("VENHA COMIGO PARA CURITIBA");
 		return;
 	}
